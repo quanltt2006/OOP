f03042fea171a71f1e2cf9c4cec2f6ed5b404876
quanltt2006 <quanltt2006@gmail.com>
2025-09-19 12:07:34 +0700

    Upload toàn bộ folder OOP

---
+++ HW1/IntroductionJava.java
@@ -0,0 +1,25 @@
+public class IntroductionJava {
+    public static void main(String[] args) {
+        System.out.print('\t');
+        System.out.println("Họ và tên: Đỗ Hồng Quân");
+        System.out.print('\t');
+        System.out.println("MSV: 24021602");
+        System.out.print('\t');
+        System.out.println("Lớp: CS5");
+        System.out.print('\t');
+        System.out.println("Github: quanltt2006");
+        System.out.print('\t');
+        System.out.println("Email: 24021602@vnu.edu.vn");
+        for (int i = 9 ; i >= 1 ; i--) {
+            System.out.print('\t');
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.print('\t');
+            System.out.println("Take one down, pass it around,");
+
+        }
+        System.out.print('\t');
+        System.out.println("No more bottles of beer on the wall.");
+
+    }
+
+}
